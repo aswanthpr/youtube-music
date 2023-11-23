6b3a0f4fd820a63afa20891713ac7c710287eb17
aswanthpr <150140594+aswanthpr@users.noreply.github.com>
2023-11-24 01:31:11 +0530

    Add files via upload

---
+++ youtubemusic.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $("#photo").click(function(){
+        alert("Welcome to YouTube music")
+
+    })
+    $(".panel-link").click(function(){
+       
+        $(this).css({background:"white", color:"black"})
+
+
+    })
+})
